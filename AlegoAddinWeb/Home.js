e9e6cbd201f1a07e0941e4aff3986154bc216063
Júlio Campos <jcserracampos@gmail.com>
2018-11-03 19:32:55 -0300

    Remove unnecessary code

--- AlegoAddinWeb/Home.js
+++ AlegoAddinWeb/Home.js
@@ -14,94 +14,23 @@
 
             // Se não estiver usando o Word 2016, use a lógica de fallback.
             if (!Office.context.requirements.isSetSupported('WordApi', '1.1')) {
-                $("#template-description").text("Este exemplo exibe o texto selecionado.");
+                $("#template-description").text("Este add-in formata o documento para os padrões da Alego.");
                 $('#button-text').text("Exibir!");
                 $('#button-desc').text("Exibir o texto selecionado");
                 
-                $('#highlight-button').click(displaySelectedText);
+                $('#header-button').click(createHeader);
                 return;
             }
 
-            $("#template-description").text("Este exemplo realça a palavra mais longa no texto que você selecionou no documento.");
-            $('#button-text').text("Realçar!");
-            $('#button-desc').text("Realça a palavra mais longa.");
+            $("#template-description").text("Este add-in formata o documento para os padrões da Alego");
+            $('#button-text').text("Criar cabeçalho!");
+            $('#button-desc').text("Insere o cabeçalho no padrão Alego.");
             
-            loadSampleData();
 
-            // Adicione um manipulador de eventos de clique ao botão de realce.
-            $('#highlight-button').click(hightlightLongestWord);
-
+            // Adicione um manipulador de eventos de clique ao botão.
             $('#header-button').click(createHeader);
         });
     };
-
-    function loadSampleData() {
-        // Execute uma operação em lote com base no modelo de objeto do Word.
-        Word.run(function (context) {
-            // Crie um objeto de proxy para o corpo do documento.
-            var body = context.document.body;
-
-            // Coloque um comando na fila para limpar o conteúdo do corpo.
-            body.clear();
-            // Coloque um comando na fila para inserir texto no final do corpo do documento do Word.
-            body.insertText(
-                "This is a sample text inserted in the document",
-                Word.InsertLocation.end);
-
-            // Sincronize o estado do documento executando os comandos na fila e retorne uma promessa para indicar a conclusão da tarefa.
-            return context.sync();
-        })
-        .catch(errorHandler);
-    }
-
-    function hightlightLongestWord() {
-        Word.run(function (context) {
-            // Coloque um comando na fila para obter a seleção atual e, em seguida,
-            // crie um objeto de intervalo de proxy com os resultados.
-            var range = context.document.getSelection();
-            
-            // Essa variável manterá os resultados da pesquisa para a palavra mais longa.
-            var searchResults;
-            
-            // Coloque um comando na fila para carregar o resultado da seleção de intervalo.
-            context.load(range, 'text');
-
-            // Sincronize o estado do documento executando os comandos na fila
-            // e retorne uma promessa para indicar a conclusão da tarefa.
-            return context.sync()
-                .then(function () {
-                    // Obtenha a palavra mais longa da seleção.
-                    var words = range.text.split(/\s+/);
-                    var longestWord = words.reduce(function (word1, word2) { return word1.length > word2.length ? word1 : word2; });
-
-                    // Coloque um comando de pesquisa na fila.
-                    searchResults = range.search(longestWord, { matchCase: true, matchWholeWord: true });
-
-                    // Coloque um comando na fila para carregar a propriedade de fonte dos resultados.
-                    context.load(searchResults, 'font');
-                })
-                .then(context.sync)
-                .then(function () {
-                    // Coloque um comando na fila para realçar os resultados da pesquisa.
-                    searchResults.items[0].font.highlightColor = '#FFFF00'; // Amarelo
-                    searchResults.items[0].font.bold = true;
-                })
-                .then(context.sync);
-        })
-        .catch(errorHandler);
-    } 
-
-
-    function displaySelectedText() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('O texto selecionado é:', '"' + result.value + '"');
-                } else {
-                    showNotification('Erro:', result.error.message);
-                }
-            });
-    }
 
     function createHeader() {
         // Run a batch operation against the Word object model.
